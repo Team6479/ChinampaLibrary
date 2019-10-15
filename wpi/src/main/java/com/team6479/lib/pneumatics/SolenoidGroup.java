cd68a87af3ab1d77a19fbe89814120e498881cd8
iboyperson <tjquillan@gmail.com>
2019-10-15 16:20:19 -0700

    Add JavaDoc to (Double)SolenoidGroup

--- wpi/src/main/java/com/team6479/lib/pneumatics/SolenoidGroup.java
+++ wpi/src/main/java/com/team6479/lib/pneumatics/SolenoidGroup.java
@@ -5,12 +5,17 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 
 /**
- * SolenoidGroup
+ * Allows multiple {@link Solenoid} objects to be linked together.
  */
 public class SolenoidGroup extends SendableBase {
   private final Solenoid[] solenoids;
   private static int instances;
 
+  /**
+   * Create a new SolenoidGroup with the provided {@link Solenoid} objects.
+   *
+   * @param solenoids The {@link Solenoid} objects to add
+   */
   public SolenoidGroup(Solenoid... solenoids) {
     this.solenoids = solenoids;
 
@@ -22,28 +27,54 @@ public class SolenoidGroup extends SendableBase {
     setName("SolenoidGroup", instances);
   }
 
+  @Override
   public void close() {
     for (Solenoid solenoid : solenoids) {
       solenoid.close();
     }
   }
 
+  /**
+   * Set the value of a solenoid.
+   *
+   * @param on True will turn the solenoid output on. False will turn the solenoid output off.
+   */
   public void set(boolean on) {
     for (Solenoid solenoid : solenoids) {
       solenoid.set(on);
     }
   }
 
+  /**
+   * Read the current value of the solenoid.
+   *
+   * @return True if the solenoid output is on or false if the solenoid output is off.
+   */
   public boolean get() {
     return solenoids[0].get();
   }
 
+  /**
+   * Set the pulse duration in the PCM. This is used in conjunction with
+   * the startPulse method to allow the PCM to control the timing of a pulse.
+   * The timing can be controlled in 0.01 second increments.
+   *
+   * @param durationSeconds The duration of the pulse, from 0.01 to 2.55 seconds.
+   *
+   * @see #startPulse()
+   */
   public void setPulseDuration(double durationSeconds) {
     for (Solenoid solenoid : solenoids) {
       solenoid.setPulseDuration(durationSeconds);
     }
   }
 
+  /**
+   * Trigger the PCM to generate a pulse of the duration set in
+   * setPulseDuration.
+   *
+   * @see #setPulseDuration(double)
+   */
   public void startPulse() {
     for (Solenoid solenoid : solenoids) {
       solenoid.startPulse();
