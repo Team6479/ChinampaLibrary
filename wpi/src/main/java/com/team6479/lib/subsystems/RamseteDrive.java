955471f9e68db9296c80ae1190619e77270e5209
Aiden Onstott <aidenonstott@gmail.com>
2020-01-31 17:15:50 -0700

    Forgot to format with google-java

--- wpi/src/main/java/com/team6479/lib/subsystems/RamseteDrive.java
+++ wpi/src/main/java/com/team6479/lib/subsystems/RamseteDrive.java
@@ -7,13 +7,22 @@ import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
 public interface RamseteDrive {
   public Pose2d getPose();
+
   public DifferentialDriveWheelSpeeds getWheelSpeeds();
+
   public void tankDriveVolts(double leftVolts, double rightVolts);
+
   public void resetEncoders();
+
   public double getLeftEncoderPos();
+
   public double getRightEncoderPos();
+
   public double getLeftEncoderVel();
+
   public double getRightEncoderVel();
+
   public double getHeading();
+
   public RamseteCommand getRamseteCommand(Trajectory trajectory);
 }
